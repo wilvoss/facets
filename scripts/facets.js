fbf798601e4f96fbc7a0765f952bace1dbef6eaf
wilvoss <wilv@allovus.com>
2023-12-29 22:23:44 -0600

    0.1.037 - web app prep

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.036',
+    version: '0.1.037',
     gameName: 'Facets',
     gameCatchphrase: 'A game of word association!',
     gameMode: 'both',
