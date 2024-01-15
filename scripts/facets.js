42f238570470132c65371407f061b8f1a6fc76d2
wilvoss <wilv@allovus.com>
2024-01-15 10:34:55 -0800

    0.1.077 - visual bug fixes

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.076',
+    version: '0.1.077',
     gameName: 'Facets',
     currentGameID: 0,
     gameCatchphrase: 'A game of words!',
