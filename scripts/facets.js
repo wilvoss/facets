8334cd8620f2c8d9c98a653d896668545ce24e50
wilvoss <wilv@allovus.com>
2024-01-15 09:55:21 -0800

    0.1.075 - parking spots ui

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.074',
+    version: '0.1.075',
     gameName: 'Facets',
     currentGameID: 0,
     gameCatchphrase: 'A game of words!',
