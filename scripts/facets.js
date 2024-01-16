edd8269577df40fa1a2892a886b2205f82e3df1a
wilvoss <wilv@allovus.com>
2024-01-16 12:07:04 -0800

    0.1.091 - improved wallpaper ui

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.090',
+    version: '0.1.091',
     gameName: 'Facets',
     currentGameID: 0,
     gameCatchphrase: 'A game of words!',
