9c4ecdf5e34caab57a94be6f62276e0f6d79dcdd
wilvoss <wilv@allovus.com>
2024-01-16 13:13:02 -0800

    0.1.094 - prehistory -> archeology

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.093',
+    version: '0.1.094',
     gameName: 'Facets',
     currentGameID: 0,
     gameCatchphrase: 'A game of words!',
