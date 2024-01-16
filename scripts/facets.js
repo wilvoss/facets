f6153b1c92f01472740ba49f1df079bc5c2790f5
wilvoss <wilv@allovus.com>
2024-01-15 22:31:05 -0800

    0.1.086 - turorial game control section

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.085',
+    version: '0.1.086',
     gameName: 'Facets',
     currentGameID: 0,
     gameCatchphrase: 'A game of words!',
