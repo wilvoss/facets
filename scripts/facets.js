84878cbb83d132c790af2022f89eba5baf56fb41
wilvoss <wilv@allovus.com>
2024-01-21 05:18:53 -0800

    0.1.101 - autocheck part 3

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.100',
+    version: '0.1.101',
     gameName: 'Facets',
     currentGameID: 0,
     currentGameSol: '',
