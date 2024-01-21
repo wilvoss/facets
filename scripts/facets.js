2738165af704341a296d9324174dc39ef6860193
wilvoss <wilv@allovus.com>
2024-01-21 09:14:38 -0800

    0.1.106 - small phones ui fix

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.105',
+    version: '0.1.106',
     gameName: 'Facets',
     currentGameID: 0,
     currentGameSol: '',
