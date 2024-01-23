67fda6b3c1d89fdbdb0457695433604f621903ae
wilvoss <wilv@allovus.com>
2024-01-22 22:39:48 -0800

    0.1.116 - updated tutorial image

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.115',
+    version: '0.1.116',
     gameName: 'Facets',
     currentGameID: 0,
     currentGameSol: '',
