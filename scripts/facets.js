dc6eae9676a57b8757812813171ff28ee2f24b50
wilvoss <wilv@allovus.com>
2024-01-23 03:03:00 -0800

    0.1.121 - visual tweaks

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.120',
+    version: '0.1.121',
     gameName: 'Facets',
     currentGameID: 0,
     currentGameSol: '',
