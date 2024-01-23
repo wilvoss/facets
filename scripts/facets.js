9b1be2b335a27a9e91c6c465b10461ec7aa5c866
wilvoss <wilv@allovus.com>
2024-01-22 18:26:37 -0800

    0.1.112 - tutorial text update

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.111',
+    version: '0.1.112',
     gameName: 'Facets',
     currentGameID: 0,
     currentGameSol: '',
