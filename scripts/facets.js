24ee1cf53356b9bfe28792b9e7c1092cf1c10442
wilvoss <wilv@allovus.com>
2024-01-24 22:04:17 -0800

    0.1.147 - accessibility updates

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.146',
+    version: '0.1.147',
     newVersionAvailable: false,
     gameName: 'Facets',
     currentGameID: 0,
