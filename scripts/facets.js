7683b952d05efd501e1d1a9a7f041d75227161ab
wilvoss <wilv@allovus.com>
2024-01-24 18:19:02 -0800

    0.1.142 - shorturl part 4

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.141',
+    version: '0.1.142',
     newVersionAvailable: false,
     gameName: 'Facets',
     currentGameID: 0,
@@ -477,7 +477,7 @@ var app = new Vue({
       this.ConstructURLForCurrentGame(isFinal);
 
       var corsflareUrl = 'https://worker-cold-butterfly-c870.bigtentgames.workers.dev/';
-      var requestUrl = corsflareUrl + encodeURIComponent(this.shareURL);
+      var requestUrl = corsflareUrl + this.shareURL;
 
       fetch(requestUrl, {
         headers: {
