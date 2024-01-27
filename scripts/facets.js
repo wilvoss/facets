f7cb10721cf62093819ad2b3278a79e529b54007
wilvoss <wilv@allovus.com>
2024-01-27 08:23:42 -0800

    0.1.156 - ui tweak (modal transitions)

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.155',
+    version: '0.1.156',
     newVersionAvailable: false,
     gameName: 'Facets',
     currentGameID: 0,
