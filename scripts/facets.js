39b7436f54f1cab4d7f8c588d1c23d5a214806de
wilvoss <wilv@allovus.com>
2024-01-29 13:03:21 -0800

    0.1.171 - (share->) bug fix

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app'];
 var app = new Vue({
   el: '#app',
   data: {
-    version: '0.1.170',
+    version: '0.1.171',
     newVersionAvailable: false,
     gameName: 'Facets',
     currentGameID: 0,
