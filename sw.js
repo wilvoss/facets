b305b7e7cfdd444d317b4ac99f2383aa45efb4bc
wilvoss <wilv@allovus.com>
2024-01-29 13:07:02 -0800

    0.1.172 - ui fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.1.171';
+const CACHE_VERSION = '0.1.172';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // these are the routes we are going to cache for offline support
