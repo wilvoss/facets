9f1a29e88228132c93c3b9cca86a520ca11d15b3
wilvoss <wilv@allovus.com>
2024-02-01 09:03:04 -0800

    0.1.180 - emoji wallpaper - reorg files - removed manifest link

--- models/WordSetObject.js
+++ models/WordSetObject.js
@@ -6,7 +6,7 @@ class WordSetObject {
     this.name = spec.name === undefined ? '' : spec.name;
     this.isSelected = spec.isSelected === undefined ? false : spec.isSelected;
     this.data = spec.data === undefined ? [] : spec.data;
-    this.textureImage = spec.textureImage === undefined ? '../images/common-alt2.jpg' : spec.textureImage;
+    this.textureImage = spec.textureImage === undefined ? '../images/wallpapers/common.jpg' : spec.textureImage;
     this.textureHue = spec.textureHue === undefined ? 205 : spec.textureHue;
     this.textureSize = spec.textureSize === undefined ? '512px' : spec.textureSize;
     this.textureBlendMode = spec.textureBlendMode === undefined ? 'normal' : spec.textureBlendMode;
@@ -18,18 +18,18 @@ class WordSetObject {
   }
 }
 
-let Nouns = new WordSetObject({ id: '1', name: 'Nouns', data: ['./data/nouns.json'], textureImage: '../images/nouns-alt2.jpg' });
-let Verbs = new WordSetObject({ id: '2', name: 'Verbs', data: ['./data/verbs.json'], textureImage: '../images/verbs-alt2.jpg' });
+let Nouns = new WordSetObject({ id: '1', name: 'Nouns', data: ['./data/nouns.json'], textureImage: '../images/wallpapers/nouns.jpg' });
+let Verbs = new WordSetObject({ id: '2', name: 'Verbs', data: ['./data/verbs.json'], textureImage: '../images/wallpapers/verbs.jpg' });
 let Winter = new WordSetObject({ id: '3', name: 'Winter', data: ['./data/winter.json'], enabled: false });
 let Spring = new WordSetObject({ id: '4', name: 'Spring', data: ['./data/spring.json'], enabled: false });
 let Summer = new WordSetObject({ id: '5', name: 'Summer', data: ['./data/summer.json'], enabled: false });
 let Fall = new WordSetObject({ id: '6', name: 'Fall', data: ['./data/fall.json'], enabled: false });
-let Science = new WordSetObject({ id: '7', name: 'Science', data: ['./data/science.json'], textureImage: '../images/science-alt2.jpg' });
-let Entertainment = new WordSetObject({ id: '8', name: 'Entertainment', startsWithVowel: true, data: ['./data/entertainment.json'], textureImage: '../images/entertainment-alt2.jpg' });
-let CommonEmoji = new WordSetObject({ id: '9', name: 'Emoji (beta)', startsWithVowel: true, data: ['./data/common-emoji.json'], scale: 1.9, wordAlignement: 'start', message: 'Some Emoji might not show on your device.' });
-let NounsVerbs = new WordSetObject({ id: '100', name: 'Nouns & Verbs', isSelected: true, data: [Nouns.data, Verbs.data], textureImage: '../images/common-alt2.jpg' });
-let Seasons = new WordSetObject({ id: '101', name: 'Seasons', data: [Winter.data, Spring.data, Summer.data, Fall.data], textureImage: '../images/seasons-alt2.jpg' });
-let Civilization = new WordSetObject({ id: '102', name: 'Civilization', data: ['./data/civilization.json'], textureImage: '../images/civilization-alt2.jpg' });
-let Archeology = new WordSetObject({ id: '103', name: 'Archeology', startsWithVowel: true, data: ['./data/prehistory.json'], textureImage: '../images/prehistory-alt2.jpg' });
+let Science = new WordSetObject({ id: '7', name: 'Science', data: ['./data/science.json'], textureImage: '../images/wallpapers/wallpapers/science.jpg' });
+let Entertainment = new WordSetObject({ id: '8', name: 'Entertainment', startsWithVowel: true, data: ['./data/entertainment.json'], textureImage: '../images/wallpapers/entertainment.jpg' });
+let CommonEmoji = new WordSetObject({ id: '9', name: 'Emoji (beta)', startsWithVowel: true, data: ['./data/common-emoji.json'], scale: 1.9, wordAlignement: 'start', message: 'Some Emoji might not show on your device.', textureImage: '../images/wallpapers/emoji.jpg'  });
+let NounsVerbs = new WordSetObject({ id: '100', name: 'Nouns & Verbs', isSelected: true, data: [Nouns.data, Verbs.data], textureImage: '../images/wallpapers/common.jpg' });
+let Seasons = new WordSetObject({ id: '101', name: 'Seasons', data: [Winter.data, Spring.data, Summer.data, Fall.data], textureImage: '../images/wallpapers/wallpapers/seasons.jpg' });
+let Civilization = new WordSetObject({ id: '102', name: 'Civilization', data: ['./data/civilization.json'], textureImage: '../images/wallpapers/civilization.jpg' });
+let Archeology = new WordSetObject({ id: '103', name: 'Archeology', startsWithVowel: true, data: ['./data/prehistory.json'], textureImage: '../images/wallpapers/prehistory.jpg' });
 
 let WordSets = [NounsVerbs, Nouns, Verbs, Winter, Spring, Summer, Fall, Seasons, Entertainment, Science, Civilization, Archeology, CommonEmoji];
