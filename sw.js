82e5b8381d12da2a23d4daa69b3f5f43a10bf533
wilvoss <wilv@allovus.com>
2024-02-05 08:04:04 -0800

    0.1.194 - short url part 10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.1.193';
+const CACHE_VERSION = '0.1.194';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // these are the routes we are going to cache for offline support
