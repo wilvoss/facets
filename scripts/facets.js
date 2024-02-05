3c829c2b0a8c606c1c48842dccaf02aee544f6b9
wilvoss <wilv@allovus.com>
2024-02-05 08:13:22 -0800

    0.1.195 - short url part 11

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '0.1.194',
+    appDataVersion: '0.1.195',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
