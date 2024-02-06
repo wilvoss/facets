838affadf3b66fef0ce5c0c4a3dafa94e486b13d
wilvoss <wilv@allovus.com>
2024-02-06 14:34:49 -0800

    1.0.001 - more credits

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.000',
+    appDataVersion: '1.0.001',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
