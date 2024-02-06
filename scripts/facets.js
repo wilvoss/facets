3a6411ddc372fa6a5dd71b38a37bdae173062d07
wilvoss <wilv@allovus.com>
2024-02-06 15:30:02 -0800

    1.0.004 - ui text tweak

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.003',
+    appDataVersion: '1.0.004',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
