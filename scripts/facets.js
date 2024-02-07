c109ca9f7f8c5afd860862a424660ef55dd457b8
wilvoss <wilv@allovus.com>
2024-02-07 10:38:13 -0800

    1.0.011 - bug fix

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.010',
+    appDataVersion: '1.0.011',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.0.010';
+const CACHE_VERSION = '1.0.011';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // these are the routes we are going to cache for offline support
