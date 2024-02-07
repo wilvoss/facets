f95c66d3d0c20db27be0ebae93574139746d8272
wilvoss <wilv@allovus.com>
2024-02-07 15:13:31 -0800

    1.0.017 - visual tweak

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.016',
+    appDataVersion: '1.0.017',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
