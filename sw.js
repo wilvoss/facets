5fcabc878ae384efb516978a9b2db230bfef6883
wilvoss <wilv@allovus.com>
2024-02-08 11:49:40 -0800

    1.0.034 - test sw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.0.033';
+const CACHE_VERSION = '1.0.034';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // these are the routes we are going to cache for offline support
