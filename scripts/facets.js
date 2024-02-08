5e878c19eaed6ce0cb8d5ff3a705feaed9b7c604
wilvoss <wilv@allovus.com>
2024-02-08 12:09:23 -0800

    1.0.037 - test

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.036',
+    appDataVersion: '1.0.037',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
