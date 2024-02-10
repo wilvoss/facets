f05c664c8c6d6e5357a25384821e1fc3fbf36287
wilvoss <wilv@allovus.com>
2024-02-09 19:28:10 -0800

    1.0.062 - fix for #3

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.061',
+    appDataVersion: '1.0.062',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
@@ -921,7 +921,7 @@ var app = new Vue({
       let _shareObject = {
         text: _text,
       };
-      if (navigator.share) {
+      if (navigator.share && navigator.canShare(_shareObject)) {
         await navigator
           .share(_shareObject)
           .then((result) => {
