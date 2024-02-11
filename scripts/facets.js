be943a47763632752cfef5b366fdc8c263a0801c
wilvoss <wilv@allovus.com>
2024-02-11 14:51:00 -0800

    1.0.092 - bug fix

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.091',
+    appDataVersion: '1.0.092',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
@@ -938,6 +938,7 @@ var app = new Vue({
 
     async ShareText(_text, _url) {
       note('ShareText() called with this text:');
+      this.appDataMessage = '';
       note(_text);
       note(_url);
 
