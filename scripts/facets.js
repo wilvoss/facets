306cb58b995afb73e544a3d8c55a9c1054db2f97
wilvoss <wilv@allovus.com>
2024-02-11 14:41:10 -0800

    1.0.091 - sw handling

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.090',
+    appDataVersion: '1.0.091',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
@@ -845,15 +845,17 @@ var app = new Vue({
     },
 
     HandleServiceWorkerUnregistration() {
-      navigator.serviceWorker.getRegistrations().then(function (registrations) {
-        // Loop through each registration
-        for (let registration of registrations) {
-          // Unregister the service worker
-          registration.unregister();
-          // Reload the page to remove the service worker
-          registration.active.postMessage('SKIP_WAITING');
-        }
-      });
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistrations().then(function (registrations) {
+          // Loop through each registration
+          for (let registration of registrations) {
+            // Unregister the service worker
+            registration.unregister();
+            // Reload the page to remove the service worker
+            registration.active.postMessage('SKIP_WAITING');
+          }
+        });
+      }
     },
 
     HandleServiceWorkerRegistration() {
