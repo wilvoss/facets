020959c656aa821f5547a2ed1b6b0344374415a1
wilvoss <wilv@allovus.com>
2024-02-11 08:23:28 -0800

    1.0.073 - ui tweak

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.072',
+    appDataVersion: '1.0.073',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
@@ -267,7 +267,8 @@ var app = new Vue({
       this.appDataCardsParked.push(new CardObject({}));
 
       this.appDataCards = temp;
-      this.ShareBoard();
+      await this.ShareBoard();
+      this.GetLast10GlobalCreatedGames();
     },
 
     async RestoreGame(_boardArray) {
