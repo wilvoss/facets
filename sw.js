f55111c2824a9c807ded46f16eb8219c87c83eb9
wilvoss <wilv@allovus.com>
2024-02-13 12:09:59 -0800

    1.0.101 - massive html/css restructure

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.0.100';
+const CACHE_VERSION = '1.0.101';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // these are the routes we are going to cache for offline support
