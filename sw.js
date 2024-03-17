7dc72893c460fa112d2b75f212872e5a773e8fdd
wilvoss <wilv@allovus.com>
2024-03-17 16:11:51 -0700

    1.0.121 - ux tweak

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1.0.118';
+const CACHE_VERSION = '1.0.121';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // these are the routes we are going to cache for offline support
