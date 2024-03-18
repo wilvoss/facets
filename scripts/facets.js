6f73fb3015ce9365d206b8b9b0075f5520375982
wilvoss <wilv@allovus.com>
2024-03-18 10:24:04 -0700

    1.0.125 - ux improvement game creation options all moved to pre-game modal

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.124',
+    appDataVersion: '1.0.125',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
