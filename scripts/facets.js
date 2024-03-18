b8313dc79cb577a6f94fcfbfe37d7b9015da6f0c
wilvoss <wilv@allovus.com>
2024-03-17 17:42:23 -0700

    1.0.122 - fonts

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.121',
+    appDataVersion: '1.0.122',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
