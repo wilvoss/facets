6752fecb3f2ef9e0ba199d123f876de9c04cefa0
wilvoss <wilv@allovus.com>
2024-10-08 19:14:46 -0700

    1.0.137 - better focus mode ux

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.136',
+    appDataVersion: '1.0.137',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
