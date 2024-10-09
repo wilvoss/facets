1fde1414d0105b8bb22ba1c2124102bfeef21dd9
wilvoss <wilv@allovus.com>
2024-10-08 19:23:23 -0700

    1.0.140 - focus bug fix?

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.139',
+    appDataVersion: '1.0.140',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
