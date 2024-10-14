e141cb6e0c8a267bb563cf6ce615c29da2efa737
wilvoss <wilv@allovus.com>
2024-10-14 08:47:45 -0700

    1.0.167 - improved simple ux

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.0.166',
+    appDataVersion: '1.0.167',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
