410767d65402161174e7652d100674e430dfb413
wilvoss <wilv@allovus.com>
2024-10-26 14:44:02 -0700

    1.1.009 - ux tweaks

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.1.008',
+    appDataVersion: '1.1.009',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
