56b13eacf1118731150904605f8a14e3dd809a19
wilvoss <wilv@allovus.com>
2024-10-26 10:53:46 -0700

    1.1.004 - small text change

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.1.003',
+    appDataVersion: '1.1.004',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
