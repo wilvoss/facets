50caca2fa4194c6a30c76fa6387c3aaaf2ef283b
wilvoss <wilv@allovus.com>
2024-10-26 17:16:43 -0700

    1.1.011 - boo, hiss

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.1.010',
+    appDataVersion: '1.1.011',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
