bef5b9ab00ca8d03ac107a8ed9843ab53220a81a
wilvoss <wilv@allovus.com>
2024-10-28 12:50:06 -0700

    1.1.033 - stubbed in poorly translated spanish words - the intent is to get these vetted/curated

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.1.032',
+    appDataVersion: '1.1.033',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
