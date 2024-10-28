e01f68ee0bf4ab57db0124293b17ab372fc999dc
wilvoss <wilv@allovus.com>
2024-10-27 18:37:02 -0700

    1.1.021 - transition ux tweaks

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.1.020',
+    appDataVersion: '1.1.021',
     appDataCards: [],
     appDataCardsParked: [],
     appDataConfirmationObject: { message: 'Did they have the right answer?', target: 'correct' },
