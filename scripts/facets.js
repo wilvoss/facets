b9936a87edbe64a511baade4b2dc71bb6252ef45
wilvoss <wilv@allovus.com>
2024-10-29 03:40:58 -0700

    1.2.009 - o.O

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.008',
+    appDataVersion: '1.2.009',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
