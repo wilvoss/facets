db7308a3f3ab9f9221ec2c8ea4274b2993d4cbb4
wilvoss <wilv@allovus.com>
2024-10-29 18:41:36 -0700

    1.2.019 - improved stats ui

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.018',
+    appDataVersion: '1.2.019',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
