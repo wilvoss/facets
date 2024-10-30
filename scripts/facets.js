1ec556f266bb22a570320c41ab0fc041ac97a2a2
wilvoss <wilv@allovus.com>
2024-10-30 06:46:23 -0700

    1.2.025 - scroll bug fix - for real this time

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.024',
+    appDataVersion: '1.2.025',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
