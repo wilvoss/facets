2552dc72990a36b9a6634ff81084b536ae43f180
wilvoss <wilvoss@users.noreply.github.com>
2024-11-01 14:45:21 -0700

    1.2.048 - stat ui update

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.047',
+    appDataVersion: '1.2.048',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
