b724f41cd9ef96834ce1d30ee80d9e3ddc3cf5f9
wilvoss <wilvoss@users.noreply.github.com>
2024-11-01 14:48:06 -0700

    1.2.049 - text change

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.048',
+    appDataVersion: '1.2.049',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
