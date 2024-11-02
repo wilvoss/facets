066ade14aad78a0981ee68b552c3aa5e3068af97
wilvoss <wilvoss@users.noreply.github.com>
2024-11-02 16:27:29 -0700

    1.2.051 - improvements

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.050',
+    appDataVersion: '1.2.051',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
