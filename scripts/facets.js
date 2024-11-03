165a1dbccb1fcd352c34150fb9a0f2b450b68c9f
wilvoss <wilv@allovus.com>
2024-11-02 22:07:04 -0700

    1.2.053 - tablet ux tweak

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.052',
+    appDataVersion: '1.2.053',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
