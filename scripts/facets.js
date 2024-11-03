1107a1ebcbfc01c8c2c4d1e41fcd24fe1bb058dd
wilvoss <wilv@allovus.com>
2024-11-02 22:09:50 -0700

    1.2.054 - redundant language files removed - ux tweak

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.053',
+    appDataVersion: '1.2.054',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
