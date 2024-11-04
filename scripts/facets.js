ceebe96a5dcffe65aafba7a1dc653974f6bbaad2
wilvoss <wilv@allovus.com>
2024-11-04 10:24:01 -0800

    1.2.059 - improved ux

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.058',
+    appDataVersion: '1.2.059',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
