bfc02f9bfa542c0b76b08816679986a084b66a9c
wilvoss <wilv@allovus.com>
2024-11-05 20:09:56 -0800

    1.2.061 - Cantonese added

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.060',
+    appDataVersion: '1.2.061',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
