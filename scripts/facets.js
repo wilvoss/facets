8856c582030fd05a016b9d5bb9a32a5548634b51
wilvoss <wilv@allovus.com>
2024-11-12 10:24:17 -0800

    1.2.065 - bug fix

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.064',
+    appDataVersion: '1.2.065',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
