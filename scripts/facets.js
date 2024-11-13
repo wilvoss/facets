a82a05349d2a564e57c9321d03996b6260676186
wilvoss <wilvoss@me.com>
2024-11-12 16:39:45 -0800

    1.2.068 - text fix

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.067',
+    appDataVersion: '1.2.068',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
