c18ac1d9415f8e3c6f31ed019804d07c866ddd38
wilvoss <wilv@allovus.com>
2024-11-19 11:49:51 -0800

    1.2.075 - new icon attempt #1

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.074',
+    appDataVersion: '1.2.075',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
