294e23fb3c123efefcdb90129f6a0f092a82a48c
wilvoss <wilv@allovus.com>
2024-11-19 17:23:08 -0800

    1.2.86 - ux tweaks

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.84',
+    appDataVersion: '1.2.86',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
