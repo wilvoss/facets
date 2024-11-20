4292a90c186e409386b725c4dc1a2fd0f0200a0d
wilvoss <wilv@allovus.com>
2024-11-19 17:28:37 -0800

    1.2.87 - tutorial tweak

--- scripts/facets.js
+++ scripts/facets.js
@@ -14,7 +14,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '1.2.86',
+    appDataVersion: '1.2.87',
     appDataCards: [],
     appDataCardsParked: [],
     appDataLanguages: AllLanguages,
