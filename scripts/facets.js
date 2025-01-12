5e1558920a256bba45e703986e42878f37b9736c
wilvoss <wilv@allovus.com>
2025-01-11 19:45:46 -0800

    2.0.12 - doubling down on timing

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '2.0.11',
+    appDataVersion: '2.0.12',
     appDataActionButtonTexts: { send: 'Send', guess: 'Guess', check: 'Check', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share' },
     appDataCards: [],
     appDataCardsParked: [],
