acb0b7da1bb8b09e0758d1ae92107696e05ffbbf
wilvoss <wilv@allovus.com>
2025-01-11 19:58:06 -0800

    2.0.15 - action tweak

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '2.0.14',
+    appDataVersion: '2.0.15',
     appDataActionButtonTexts: { send: 'Send', guess: 'Guess', check: 'Check', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share' },
     appDataCards: [],
     appDataCardsParked: [],
