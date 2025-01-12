6b55e4d6ab78b3900237ce39e76f93a7c4cbe5a7
wilvoss <wilv@allovus.com>
2025-01-11 20:42:52 -0800

    removing sandbox from puppeteer

--- puppeteer.js
+++ puppeteer.js
@@ -1,15 +1,21 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
 
+  // Navigate to the URL
   await page.goto('https://facets.bigtentgames.com/generate.html?generated=true');
 
+  // Wait for 2 minutes
   await new Promise((resolve) => setTimeout(resolve, 120000));
 
+  // Reload the page
   await page.reload();
 
+  // Wait for 30 seconds
   await new Promise((resolve) => setTimeout(resolve, 30000));
 
   await browser.close();
