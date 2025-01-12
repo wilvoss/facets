923dfdbe78c005c7c8ce79dca52f0c938cadc96c
wilvoss <wilv@allovus.com>
2025-01-11 22:21:05 -0800

    2.0.20 - color tweaks

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '2.0.19',
+    appDataVersion: '2.0.20',
     appDataActionButtonTexts: { send: 'Send', guess: 'Guess', check: 'Check', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share' },
     appDataCards: [],
     appDataCardsParked: [],
