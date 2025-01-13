165a1e85543729024a17ae4a73552c2dc6c3ac30
wilvoss <wilv@allovus.com>
2025-01-12 21:48:55 -0800

    2.0.27 - gamelinks are fully clickable

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '2.0.26',
+    appDataVersion: '2.0.27',
     appDataActionButtonTexts: { send: 'Send', guess: 'Guess', check: 'Check', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share' },
     appDataCards: [],
     appDataCardsParked: [],
