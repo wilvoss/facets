63eeb10b46b227a70e1a606f6dc44fafb897152b
wilvoss <wilv@allovus.com>
2025-01-14 19:07:57 -0800

    2.0.49 - longer pause in puppeteer - github console logging

--- puppeteer.js
+++ puppeteer.js
@@ -1,22 +1,23 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-  const page = await browser.newPage();
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
 
-  // Navigate to the URL
-  await page.goto('https://facets.bigtentgames.com/generate.html?generated=true');
+    // Navigate to the URL
+    await page.goto('https://facets.bigtentgames.com/generate.html?generated=true');
 
-  // Wait for 20 seconds
-  await new Promise((resolve) => setTimeout(resolve, 20000));
+    await new Promise((resolve) => setTimeout(resolve, 120000)); // Increased wait time
+    await page.reload();
+    await new Promise((resolve) => setTimeout(resolve, 120000)); // Increased wait time
+    await browser.close();
 
-  // Reload the page
-  await page.reload();
-
-  // Wait for 10 seconds
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-
-  await browser.close();
+    console.log('Script executed successfully');
+  } catch (error) {
+    console.error('Error during script execution:', error.message);
+    process.exit(1); // Ensure GitHub Action fails in case of error
+  }
 })();
