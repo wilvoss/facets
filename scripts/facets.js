57c8c3758b87991899b1a26700c860aa1450b799
wilvoss <wilv@allovus.com>
2025-01-15 09:42:43 -0800

    2.0.61 - input autocomplete="off"

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '2.0.60',
+    appDataVersion: '2.0.61',
     appDataActionButtonTexts: { send: 'Send', guess: 'Guess', check: 'Check', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
     appDataCards: [],
     appDataCardsParked: [],
