13e5479afa34afce4a1f3ae27c6b63690bc9aa26
wilvoss <wilv@allovus.com>
2025-01-20 13:38:49 -0800

    2.0.98 - Home -> Menu

--- scripts/facets.js
+++ scripts/facets.js
@@ -13,7 +13,7 @@ var app = new Vue({
   el: '#app',
   data: {
     // app data
-    appDataVersion: '2.0.97',
+    appDataVersion: '2.0.98',
     appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
     appDataCards: [],
     appDataCardsParked: [],
