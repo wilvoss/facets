0d90ae5ec8badf883a3252fa9e4c15d7f503ab8e
wilvoss <wilv@allovus.com>
2025-01-25 15:56:32 -0800

    2.1.29 - manifest versioning (inline)

--- scripts/facets.js
+++ scripts/facets.js
@@ -10,7 +10,7 @@ var app = new Vue({
   el: '#app',
   data: {
     //#region APP DATA
-    appDataVersion: '2.1.28',
+    appDataVersion: '2.1.30',
     appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
     appDataCards: [],
     appDataCardsParked: [],
