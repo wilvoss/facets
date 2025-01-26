2e4fbefe56bb54d6e13896d8842d2d0ae80a7165
wilvoss <wilv@allovus.com>
2025-01-25 21:11:33 -0800

    2.1.50 - friend solution includes their name

--- scripts/facets.js
+++ scripts/facets.js
@@ -10,7 +10,7 @@ var app = new Vue({
   el: '#app',
   data: {
     //#region APP DATA
-    appDataVersion: '2.1.49',
+    appDataVersion: '2.1.50',
     appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
     appDataCards: [],
     appDataCardsParked: [],
@@ -631,7 +631,7 @@ var app = new Vue({
           this.currentGameSolutionActual = urlParams.get('sol');
           if (urlParams.has('final')) {
             this.currentGameSolutionGuessing = this.currentGameSolutionActual;
-            this.appDataMessage = this.appDataPlayerCurrent.name + ", here's the solution.";
+            this.appDataMessage = `${this.appDataPlayerCurrent.name}, here's the solution to ${this.appDataPlayerSender.name}'s puzzle.`;
             this.currentGameReviewIsFinal = true;
           }
         } else {
