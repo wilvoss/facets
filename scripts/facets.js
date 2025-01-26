69b06f2c2ba136cbed20f762e6e758ec6841d66d
wilvoss <wilv@allovus.com>
2025-01-25 20:40:02 -0800

    2.1.47 - background color bug fix

--- scripts/facets.js
+++ scripts/facets.js
@@ -10,7 +10,7 @@ var app = new Vue({
   el: '#app',
   data: {
     //#region APP DATA
-    appDataVersion: '2.1.46',
+    appDataVersion: '2.1.47',
     appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
     appDataCards: [],
     appDataCardsParked: [],
