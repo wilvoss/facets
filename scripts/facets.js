4fe9b5c3366a40c1742431183e091f4c56228045
wilvoss <wilv@allovus.com>
2025-02-10 14:55:34 -0800

    2.1.79 - updated personal stat UI

--- scripts/facets.js
+++ scripts/facets.js
@@ -11,7 +11,7 @@ var app = new Vue({
   data() {
     return {
       //#region APP DATA
-      appDataVersion: '2.1.78',
+      appDataVersion: '2.1.79',
       appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
       appDataCards: [],
       appDataCardsParked: [],
@@ -66,6 +66,7 @@ var app = new Vue({
       appStateShowConfirmation: false,
       appStateShowGlobalCreated: false,
       appStateShowDailyGames: false,
+      appStateDailyCongrats: false,
       appStateShowInfo: false,
       appStateShowIntro: false,
       appStateShowOOBE: false,
