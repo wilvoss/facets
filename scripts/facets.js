135610a18af88643f220765b4490e34338ee7d8b
wilvoss <wilv@allovus.com>
2025-02-14 09:48:00 -0800

    2.1.88 - ui tweak

--- scripts/facets.js
+++ scripts/facets.js
@@ -11,7 +11,7 @@ var app = new Vue({
   data() {
     return {
       //#region APP DATA
-      appDataVersion: '2.1.87',
+      appDataVersion: '2.1.88',
       appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
       appDataCards: [],
       appDataCardsParked: [],
