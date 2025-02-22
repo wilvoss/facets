14eddc1495d113a75dbc9cc58f8785a26bd7ea28
wilvoss <wilv@allovus.com>
2025-02-21 17:53:25 -0800

    2.2.01 - eemoved dup 🧑‍🧑‍🧒‍🧒s

--- scripts/facets.js
+++ scripts/facets.js
@@ -11,7 +11,7 @@ var app = new Vue({
   data() {
     return {
       //#region APP DATA
-      appDataVersion: '2.2.00',
+      appDataVersion: '2.2.01',
       appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
       appDataCards: [],
       appDataCardsParked: [],
