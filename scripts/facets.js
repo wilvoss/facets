c7721cbf320d3e3a56afcece126181a71d143ed7
wilvoss <wilv@allovus.com>
2025-02-22 10:00:46 -0800

    2.2.03 - nicer beta message

--- scripts/facets.js
+++ scripts/facets.js
@@ -11,7 +11,7 @@ var app = new Vue({
   data() {
     return {
       //#region APP DATA
-      appDataVersion: '2.2.02',
+      appDataVersion: '2.2.03',
       appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
       appDataCards: [],
       appDataCardsParked: [],
