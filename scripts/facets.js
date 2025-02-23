311f923f0e22be0a986320373d373b2937bbb161
wilvoss <wilv@allovus.com>
2025-02-23 09:27:26 -0800

    2.2.07 - better ID validation

--- scripts/facets.js
+++ scripts/facets.js
@@ -11,7 +11,7 @@ var app = new Vue({
   data() {
     return {
       //#region APP DATA
-      appDataVersion: '2.2.06',
+      appDataVersion: '2.2.07',
       appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
       appDataCards: [],
       appDataCardsParked: [],
