ae8a1a2b01c5d3e42082cc582dd7b7e15516384a
wilvoss <wilv@allovus.com>
2025-02-26 11:45:15 -0800

    2.2.21 - 🚫 simplifed wrong card indication

--- scripts/facets.js
+++ scripts/facets.js
@@ -11,7 +11,7 @@ var app = new Vue({
   data() {
     return {
       //#region APP DATA
-      appDataVersion: '2.2.20',
+      appDataVersion: '2.2.21',
       appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
       appDataCards: [],
       appDataCardsParked: [],
