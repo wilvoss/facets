194ad5d2bce7bf0abbf5888d5e7bf247d9d0f009
wilvoss <wilv@allovus.com>
2025-03-28 09:13:19 -0700

    2.2.35 -  calendar icon missing fix

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ var app = new Vue({
   data() {
     return {
       //#region APP DATA
-      appDataVersion: '2.2.35',
+      appDataVersion: '2.2.36',
       appDataActionButtonTexts: { send: 'Send', guess: 'Guess', reply: 'Reply', copy: 'Copy', respond: 'Respond', create: 'Create', share: 'Share', quit: 'Give up' },
       appDataCards: [],
       appDataCardsParked: [],
