4cf2d98356bc987525ff32e64af88a5f029bc4ea
wilvoss <wilv@allovus.com>
2025-06-03 06:53:01 -0700

    2.3.10 - logo color

--- scripts/facets.js
+++ scripts/facets.js
@@ -12,7 +12,7 @@ var app = new Vue({
   data() {
     return {
       //#region APP DATA
-      appDataVersion: '2.3.09',
+      appDataVersion: '2.3.10',
       // prettier-ignore
       appDataGuessingFirstRunItems: [
         ['Hey! Your friend created this word association puzzle for you to solve.', 'Hello! Our AI created this word association puzzle for you to solve.'],
