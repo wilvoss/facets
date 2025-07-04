cc203be4d3fc352f186f010cfe32058643342222
wilvoss <wilv@allovus.com>
2025-07-04 14:00:26 -0700

    2.3.33 - app icon fix

--- scripts/facets.js
+++ scripts/facets.js
@@ -1,4 +1,4 @@
-const version = '2.3.32';
+const version = '2.3.33';
 
 //#region MODULE HANDLING
 async function loadModels() {
@@ -69,7 +69,7 @@ LoadAllModules().then((modules) => {
     data() {
       return {
         //#region APP DATA
-        appDataVersion: '2.3.32',
+        appDataVersion: '2.3.33',
         appDataGuessingFirstRunItems: modules.firstRunGuessingMessages,
         appDataCreatorFirstRunItems: modules.firstRunCreatingMessages,
         appDataReviewingFirstRunItems: modules.firstRunReviewingMessages,
