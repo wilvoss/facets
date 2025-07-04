b8c5c2b6112e041d219be134c14b708c59af461e
wilvoss <wilv@allovus.com>
2025-07-03 19:08:41 -0700

    2.3.30 - apple icon

--- scripts/facets.js
+++ scripts/facets.js
@@ -1,4 +1,4 @@
-const version = '2.3.29';
+const version = '2.3.30';
 
 //#region MODULE HANDLING
 async function loadModels() {
@@ -69,7 +69,7 @@ LoadAllModules().then((modules) => {
     data() {
       return {
         //#region APP DATA
-        appDataVersion: '2.3.29',
+        appDataVersion: '2.3.30',
         appDataGuessingFirstRunItems: modules.firstRunGuessingMessages,
         appDataCreatorFirstRunItems: modules.firstRunCreatingMessages,
         appDataReviewingFirstRunItems: modules.firstRunReviewingMessages,
