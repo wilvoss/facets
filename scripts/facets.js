e1e1d839e64e4033192f080f3bbce3b2cf010dc9
wilvoss <wilv@allovus.com>
2025-07-04 14:33:05 -0700

    2.3.34 - 1024 app icon

--- scripts/facets.js
+++ scripts/facets.js
@@ -1,4 +1,4 @@
-const version = '2.3.33';
+const version = '2.3.34';
 
 //#region MODULE HANDLING
 async function loadModels() {
@@ -69,7 +69,7 @@ LoadAllModules().then((modules) => {
     data() {
       return {
         //#region APP DATA
-        appDataVersion: '2.3.33',
+        appDataVersion: '2.3.34',
         appDataGuessingFirstRunItems: modules.firstRunGuessingMessages,
         appDataCreatorFirstRunItems: modules.firstRunCreatingMessages,
         appDataReviewingFirstRunItems: modules.firstRunReviewingMessages,
