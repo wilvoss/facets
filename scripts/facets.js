1dfd0f19671db91c2dc4dfa80049b06b64cc3637
wilvoss <wilv@allovus.com>
2025-07-04 13:09:30 -0700

    2.3.32 - icon tweak

--- scripts/facets.js
+++ scripts/facets.js
@@ -1,4 +1,4 @@
-const version = '2.3.31';
+const version = '2.3.32';
 
 //#region MODULE HANDLING
 async function loadModels() {
@@ -69,7 +69,7 @@ LoadAllModules().then((modules) => {
     data() {
       return {
         //#region APP DATA
-        appDataVersion: '2.3.31',
+        appDataVersion: '2.3.32',
         appDataGuessingFirstRunItems: modules.firstRunGuessingMessages,
         appDataCreatorFirstRunItems: modules.firstRunCreatingMessages,
         appDataReviewingFirstRunItems: modules.firstRunReviewingMessages,
