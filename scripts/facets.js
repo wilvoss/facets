388176f18e81923dfa10dddde7c1342870e62d0f
wilvoss <wilv@allovus.com>
2025-07-27 14:33:48 -0700

    2.3.37 - frosted home buttons

--- scripts/facets.js
+++ scripts/facets.js
@@ -1,4 +1,4 @@
-const version = '2.3.36';
+const version = '2.3.37';
 
 //#region MODULE HANDLING
 async function loadModels() {
@@ -69,7 +69,7 @@ LoadAllModules().then((modules) => {
     data() {
       return {
         //#region APP DATA
-        appDataVersion: '2.3.36',
+        appDataVersion: '2.3.37',
         appDataGuessingFirstRunItems: modules.firstRunGuessingMessages,
         appDataCreatorFirstRunItems: modules.firstRunCreatingMessages,
         appDataReviewingFirstRunItems: modules.firstRunReviewingMessages,
