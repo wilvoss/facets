a55a2cf4ee23c534c21211a75b017b1521e560a6
wilvoss <wilv@allovus.com>
2025-07-28 10:51:44 -0700

    2.3.41 - home page icons

--- scripts/facets.js
+++ scripts/facets.js
@@ -1,4 +1,4 @@
-const version = '2.3.40';
+const version = '2.3.41';
 
 //#region MODULE HANDLING
 async function loadModels() {
@@ -69,7 +69,7 @@ LoadAllModules().then((modules) => {
     data() {
       return {
         //#region APP DATA
-        appDataVersion: '2.3.40',
+        appDataVersion: '2.3.41',
         appDataGuessingFirstRunItems: modules.firstRunGuessingMessages,
         appDataCreatorFirstRunItems: modules.firstRunCreatingMessages,
         appDataReviewingFirstRunItems: modules.firstRunReviewingMessages,
