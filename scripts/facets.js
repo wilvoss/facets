306f3306746fbb5d0d7022c5c0c332b454871d30
wilvoss <wilv@allovus.com>
2025-07-28 15:53:29 -0700

    2.3.47 - better bowler hat

--- scripts/facets.js
+++ scripts/facets.js
@@ -1,4 +1,4 @@
-const version = '2.3.46';
+const version = '2.3.47';
 
 //#region MODULE HANDLING
 async function loadModels() {
@@ -69,7 +69,7 @@ LoadAllModules().then((modules) => {
     data() {
       return {
         //#region APP DATA
-        appDataVersion: '2.3.46',
+        appDataVersion: '2.3.47',
         appDataGuessingFirstRunItems: modules.firstRunGuessingMessages,
         appDataCreatorFirstRunItems: modules.firstRunCreatingMessages,
         appDataReviewingFirstRunItems: modules.firstRunReviewingMessages,
