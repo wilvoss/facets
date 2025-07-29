3a98163b89de0a59cfd943f28a4b6012bb2a6562
wilvoss <wilv@allovus.com>
2025-07-29 11:33:09 -0700

    2.3.51 - webp

--- constants/gameplay.js
+++ constants/gameplay.js
@@ -4,25 +4,25 @@ export async function loadGameplayModules() {
   const { WordSetObject, WordObject, LanguageObject, CardObject, PlayerObject } = await import(`../models/models.min.js?${version}`);
 
   // // Predefined WordSet instances
-  const Nouns = new WordSetObject({ id: '1', name: 'Nouns', data: ['./data/nouns.json?2.2.12'], textureImage: '../images/wallpapers/nouns.jpg', enabled: false });
-  const Verbs = new WordSetObject({ id: '2', name: 'Verbs', data: ['./data/verbs.json?2.2.06'], textureImage: '../images/wallpapers/verbs.jpg', enabled: false });
+  const Nouns = new WordSetObject({ id: '1', name: 'Nouns', data: ['./data/nouns.json?2.2.12'], textureImage: '../images/wallpapers/nouns.webp', enabled: false });
+  const Verbs = new WordSetObject({ id: '2', name: 'Verbs', data: ['./data/verbs.json?2.2.06'], textureImage: '../images/wallpapers/verbs.webp', enabled: false });
   const Winter = new WordSetObject({ id: '3', name: 'Winter', data: ['./data/winter.json?2.2.06'], enabled: false });
   const Spring = new WordSetObject({ id: '4', name: 'Spring', data: ['./data/spring.json?2.2.06'], enabled: false });
   const Summer = new WordSetObject({ id: '5', name: 'Summer', data: ['./data/summer.json?2.2.06'], enabled: false });
   const Fall = new WordSetObject({ id: '6', name: 'Fall', data: ['./data/fall.json?2.2.06'], enabled: false });
-  const Science = new WordSetObject({ id: '7', emoji: '🧪', name: 'Science', data: ['./data/science.json?2.2.06'], textureImage: '../images/wallpapers/science.jpg' });
-  const Entertainment = new WordSetObject({ id: '8', emoji: '🍿', name: 'Entertainment', startsWithVowel: true, data: ['./data/entertainment.json?2.2.06'], textureImage: '../images/wallpapers/entertainment.jpg' });
-  const CommonEmoji = new WordSetObject({ id: '9', emoji: '✨', noLanguage: true, name: 'Emoji', startsWithVowel: true, data: ['./data/common-emoji.json?2.2.06'], scale: 1.8, wordAlignement: 'start', message: 'Some Emoji might not show on your device.', textureImage: '../images/wallpapers/emoji.jpg' });
+  const Science = new WordSetObject({ id: '7', emoji: '🧪', name: 'Science', data: ['./data/science.json?2.2.06'], textureImage: '../images/wallpapers/science.webp' });
+  const Entertainment = new WordSetObject({ id: '8', emoji: '🍿', name: 'Entertainment', startsWithVowel: true, data: ['./data/entertainment.json?2.2.06'], textureImage: '../images/wallpapers/entertainment.webp' });
+  const CommonEmoji = new WordSetObject({ id: '9', emoji: '✨', noLanguage: true, name: 'Emoji', startsWithVowel: true, data: ['./data/common-emoji.json?2.2.06'], scale: 1.8, wordAlignement: 'start', message: 'Some Emoji might not show on your device.', textureImage: '../images/wallpapers/emoji.webp' });
   const Cities = new WordSetObject({ id: '10', name: 'Cities', startsWithVowel: true, data: ['./data/cities.json?2.2.06'], enabled: false });
   const Sport = new WordSetObject({ id: '11', name: 'Sport', startsWithVowel: true, data: ['./data/sports.json?2.2.06'], enabled: false });
-  const Adjectives = new WordSetObject({ id: '12', name: 'Adjectives', data: ['./data/adjectives.json?2.2.33'], textureImage: '../images/wallpapers/verbs.jpg', enabled: false });
-  const Interjections = new WordSetObject({ id: '13', name: 'Interjections', data: ['./data/interjections.json?2.2.33'], textureImage: '../images/wallpapers/verbs.jpg', enabled: false });
-  const NounsVerbsAdj = new WordSetObject({ id: '100', emoji: '🍎', name: 'Common', isSelected: true, data: [Nouns.data, Verbs.data, Adjectives.data], textureImage: '../images/wallpapers/common.jpg' });
-  const Seasons = new WordSetObject({ id: '101', emoji: '🍁', name: 'Seasons', data: [Winter.data, Spring.data, Summer.data, Fall.data], textureImage: '../images/wallpapers/seasons.jpg' });
-  const Civilization = new WordSetObject({ id: '102', emoji: '🌍', name: 'Civilization', data: ['./data/civilization.json?2.2.06'], textureImage: '../images/wallpapers/civilization.jpg' });
-  const Archeology = new WordSetObject({ id: '103', emoji: '🦴', name: 'Archeology', startsWithVowel: true, data: ['./data/prehistory.json?2.2.06'], textureImage: '../images/wallpapers/prehistory.jpg', enabled: false });
-  const Sports = new WordSetObject({ id: '104', emoji: '🏓', name: 'Sports', data: [Sport.data, Cities.data], textureImage: '../images/wallpapers/sports.jpg' });
-  const Kaomoji = new WordSetObject({ id: '105', scale: 0.8, emoji: '(☉_☉)', noLanguage: true, name: 'Kaomoji (¬_¬)', data: ['./data/kaomoji.json?2.2.06'], textureImage: '../images/wallpapers/emoji.jpg' });
+  const Adjectives = new WordSetObject({ id: '12', name: 'Adjectives', data: ['./data/adjectives.json?2.2.33'], textureImage: '../images/wallpapers/verbs.webp', enabled: false });
+  const Interjections = new WordSetObject({ id: '13', name: 'Interjections', data: ['./data/interjections.json?2.2.33'], textureImage: '../images/wallpapers/verbs.webp', enabled: false });
+  const NounsVerbsAdj = new WordSetObject({ id: '100', emoji: '🍎', name: 'Common', isSelected: true, data: [Nouns.data, Verbs.data, Adjectives.data], textureImage: '../images/wallpapers/common.webp' });
+  const Seasons = new WordSetObject({ id: '101', emoji: '🍁', name: 'Seasons', data: [Winter.data, Spring.data, Summer.data, Fall.data], textureImage: '../images/wallpapers/seasons.webp' });
+  const Civilization = new WordSetObject({ id: '102', emoji: '🌍', name: 'Civilization', data: ['./data/civilization.json?2.2.06'], textureImage: '../images/wallpapers/civilization.webp' });
+  const Archeology = new WordSetObject({ id: '103', emoji: '🦴', name: 'Archeology', startsWithVowel: true, data: ['./data/prehistory.json?2.2.06'], textureImage: '../images/wallpapers/prehistory.webp', enabled: false });
+  const Sports = new WordSetObject({ id: '104', emoji: '🏓', name: 'Sports', data: [Sport.data, Cities.data], textureImage: '../images/wallpapers/sports.webp' });
+  const Kaomoji = new WordSetObject({ id: '105', scale: 0.8, emoji: '(☉_☉)', noLanguage: true, name: 'Kaomoji (¬_¬)', data: ['./data/kaomoji.json?2.2.06'], textureImage: '../images/wallpapers/emoji.webp' });
 
   const All = new WordSetObject({
     id: '1000',
