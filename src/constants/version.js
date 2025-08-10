d391c42a4c94255290d0ede6e4bd8118e786492a
wilvoss <wilv@allovus.com>
2025-08-10 08:25:29 -0700

    2.3.74 - randomize card position bug

--- src/constants/version.js
+++ src/constants/version.js
@@ -1,1 +1,1 @@
-export const version = '2.3.73';
+export const version = '2.3.74';
--- src/facets.js
+++ src/facets.js
@@ -792,6 +792,12 @@ LoadAllModules().then((modules) => {
         }
         this.appDataCardsParked.push(new modules.CardObject({}));
 
+        // Shuffle temp before assigning
+        for (let i = temp.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [temp[i], temp[j]] = [temp[j], temp[i]];
+        }
+
         this.appDataCards = temp;
         await this.ShareBoard(false, true);
         this.GetRecentAnonymousGames();
